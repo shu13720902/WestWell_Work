bc3b010c69deb0d713c794efc86a86dd50d56621
shu13720902 <shu13720902@gmail.com>
2018-06-28 10:31:09 +0800

    Update Generate_TestImage.py

--- Generate_TestImage.py
+++ Generate_TestImage.py
@@ -7,9 +7,6 @@ After finish inputting the number or letter, we revise the pixel value of the in
 into its corresponding decimal value.
 And then put all the inputted number of letters into one test image (600*800) randomly.
 There mustn't have overlap bettwen two number or letters 
-
-@author: Oscar
-@E-mail: Xinghui.Shu@westwell-lab.com
 """
 
 #import binascii
